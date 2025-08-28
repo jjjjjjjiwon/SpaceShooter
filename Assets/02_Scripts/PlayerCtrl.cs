803f442b73e5a0733829729692b8a3462c6b325f
jjjjjjjiwon <hgw7501@gmail.com>
2025-08-28 15:49:40 +0900

    0828

--- Assets/02_Scripts/PlayerCtrl.cs
+++ Assets/02_Scripts/PlayerCtrl.cs
@@ -8,6 +8,7 @@ public class PlayerCtrl : MonoBehaviour
     // [SerializeField] - private로 쓰고 싶은데 unity에서는 보고 싶을떄 사용
     [SerializeField] Transform tr; 
     [SerializeField] public float moveSpeed = 20.0f;
+    [SerializeField] public float turnSpeed = 100.0f;
 
     void Start()
     {
@@ -23,12 +24,19 @@ public class PlayerCtrl : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
         Debug.Log($"h = {h}");
         Debug.Log($"v = {v}");
         // Transform.Position
         //transform.position += new Vector3(0, 0, 1);
         // normalized
         // tr.position += Vector3.forward * 1;
-        tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed, Space.Self); // forward는 Vector3의 Z값과 같다, 어차피 앞으로 가는
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        tr.Translate(moveDir * moveSpeed * Time.deltaTime);
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
+
+
+        //tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed, Space.Self); // forward는 Vector3의 Z값과 같다, 어차피 앞으로 가는
+
     }
 }
