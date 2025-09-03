aa5274ad1f06c7bf380eed54d14dc42b3fa53b25
jjjjjjjiwon <hgw7501@gmail.com>
2025-09-03 15:52:17 +0900

    0903

---
+++ Assets/02_Scripts/BulletCtrl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Rider.Unity.Editor;
+using UnityEditor;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    public float damage = 20.0f;
+    public float force = 1500.0f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * force);
+    }
+}
