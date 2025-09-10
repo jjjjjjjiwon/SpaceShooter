7aee8cd3b0168c65bc95c1321cb6ba310a8da6c6
jjjjjjjiwon <hgw7501@gmail.com>
2025-09-10 15:49:43 +0900

    0910

--- Assets/02_Scripts/RemoveBullet.cs
+++ Assets/02_Scripts/RemoveBullet.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
     void OnCollisionEnter(Collision collision)
     {
         //if (collision.collider.tag == "BULLET")
         //if (collision.gameObject.tag.Equals("BULLET")) // 똑같은데 Equals가 GC덜 먹음
         if (collision.gameObject.CompareTag("BULLET"))
         {
+            ContactPoint cp = collision.GetContact(0);
+            Quaternion rot = Quaternion.LookRotation(-cp.normal);
+
+            GameObject spark = Instantiate(sparkEffect, collision.transform.position, Quaternion.identity);
+            Destroy(spark, 0.5f);
             Destroy(collision.gameObject);
         }
     }
